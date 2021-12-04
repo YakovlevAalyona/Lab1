8b13a98d160f95baaf72b0514ad2cb2bc5464a9a
YakovlevAalyona <93199050+YakovlevAalyona@users.noreply.github.com>
2021-12-04 23:49:49 +0300

    Add files via upload

---
+++ _2инд.py
@@ -0,0 +1,13 @@
+import time
+x=float(input("Введите x: " ))
+y=float(input("Введите y: " ))
+z=float(input("Введите z: " ))
+
+import math
+if (1/2+math.sin(y)) !=0:
+    a=(2*math.cos(x-math.pi/6))/(1/2+math.sin(y))
+if ((3+z**2)/5) !=0:
+    b=1+(z**2)/((3+z**2)/5)
+print ("a=" +str (a))
+print ("b=" +str (b))
+
