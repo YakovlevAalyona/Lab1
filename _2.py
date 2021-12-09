4647b9f3e38ddaebe28e79ac51b2756bb6b07ea1
YakovlevAalyona <93199050+YakovlevAalyona@users.noreply.github.com>
2021-12-09 23:07:54 +0300

    Add files via upload

---
+++ _2.py
@@ -0,0 +1,6 @@
+x =int(input('Введите количество секунд: '))
+hour = x//3600
+minute=x//60
+second=x-minute*60
+print('Полных часов: ', hour, 'Полных минут: ', minute, 'Полных секунд:' ,second )
+
---
+++ _4.py
@@ -0,0 +1,9 @@
+a=int(input("Введите a: "))
+b=int(input("Введите b: "))
+a=a+b
+b=a-b
+a=a-b
+print("меняем местами")
+print("a:" + str(a))
+print("b:" + str(b))
+
