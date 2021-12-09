89a2ab48de4dd812947d0ebb4bcec88ff7e2f12f
YakovlevAalyona <93199050+YakovlevAalyona@users.noreply.github.com>
2021-12-09 23:04:04 +0300

    Delete _2.py

--- _2.py
+++
@@ -1,7 +0,0 @@
-import time
-x =int(input('Введите количество секунд: '))
-hour = x//3600
-minute=x//60
-second=x-minute*60
-print('Полных часов: ', hour, 'Полных минут: ', minute, 'Полных секунд:' ,second )
-time.sleep(10000)
